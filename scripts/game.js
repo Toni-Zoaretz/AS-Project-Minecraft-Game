73ff22851f0054925a191ed123a4d38d42e98520
Toni-Zoaretz <tonizoaretz1@gmail.com>
2023-02-03 01:14:48 +0200

    map

--- scripts/game.js
+++ scripts/game.js
@@ -7,3 +7,36 @@
 // event for return main page
 // event for new game
 // event for every cube in the map
+
+const mainGame = document.querySelector(".main-game");
+
+function createMap(type, rowStart, rowEnd, columnStart, columnEnd) {
+  for (let row = rowStart; row <= rowEnd; row++) {
+    for (let column = columnStart; column <= columnEnd; ++column) {
+      const materialElement = document.createElement("div");
+      materialElement.style.gridRowStart = row;
+      materialElement.style.gridColumnStart = column;
+      materialElement.classList.add(`${type}`);
+      mainGame.appendChild(materialElement);
+    }
+  }
+}
+
+createMap("sun", 2, 5, 2, 5);
+createMap("sun", 3, 4, 2, 5);
+createMap("dirt", 16, 21, 1, 21);
+createMap("grass", 15, 15, 1, 21);
+createMap("gravel", 12, 14, 18, 18);
+createMap("gravel", 13, 14, 3, 3);
+createMap("gravel", 14, 14, 11, 11);
+createMap("trunck_tree", 12, 14, 6, 6);
+createMap("trunck_tree", 11, 14, 15, 15);
+createMap("leaves", 11, 12, 20, 20);
+
+// leaves;
+// trunck_tree;
+// grass;
+// dirt;
+// gravel;
+// sky;
+// sun;
