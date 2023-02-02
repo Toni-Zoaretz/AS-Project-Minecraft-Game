ffaa0b6e72a06123a1e8594f7f6b88243b44864a
Toni-Zoaretz <tonizoaretz1@gmail.com>
2023-02-02 23:04:37 +0200

    only styling

---
+++ scripts/game.js
@@ -0,0 +1,9 @@
+// target all page elements
+// function to check if he use the right tool for the right element etc axe shovel
+// function to count the item he picked
+// function to collect and items  he packed to the amount boxes side bar
+// function to pull out from the amount box and drip in on the screen
+// event for the start game
+// event for return main page
+// event for new game
+// event for every cube in the map
