eb048aaa9535926ce7f7b0d448c796cc21bbea53
Toni-Zoaretz <tonizoaretz1@gmail.com>
2023-02-04 20:08:21 +0200

    final project

--- scripts/game.js
+++ scripts/game.js
@@ -8,8 +8,5 @@ const flowers = document.querySelector("#flowers"); //trash
 const startAgain = document.querySelector(".btn-start"); //btn-start
 const returnMainScreen = document.querySelector(".btn-return-main"); //btn-main-screen
 const axe = document.querySelector("#axe"); //axe
-console.log(axe.textContent);
 const pickaxe = document.querySelector("#pickaxe"); //pikaxe
-console.log(pickaxe.textContent);
 const shovel = document.querySelector("#shovel"); //shovel
-console.log(shovel.textContent);
